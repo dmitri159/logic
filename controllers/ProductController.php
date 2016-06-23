dd0cf652d87527f806261e467411d16a31814e25
keei <cheah.ky91@gmail.com>
2016-06-23 21:46:38 +0800

    improve mvc code

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -9,16 +9,7 @@ use app\models\Promocode;
 
 class ProductController extends Controller
 {
-    protected static $MALAYSIA = 10;
-    protected static $SINGAPORE = 20;
-    protected static $BRUNEI = 25;
-
-    protected static $cart_total = array (
-        'total' => 0,
-        'total_b' => 0,
-        'shipping' => 0,
-        'discount' => 0
-    );
+    
 
     public function actionIndex()
     {
@@ -41,61 +32,15 @@ class ProductController extends Controller
     public function actionCart()
     {   
         $post = Yii::$app->request->post();
+        $query = Product::calculateCart($post);
         $model = Yii::$app->cart->getPositions();
-        $count = Yii::$app->cart->getCount();
-        $total = Yii::$app->cart->getCost();
-        self::$cart_total['total_b'] = $total;
-        $query = '';
-        if($post) {
-            if($post['validate']) {
-                $promocode = strtoupper($post['promocode']);
-                $country = $post['country'];
-                $query = Promocode::find()->where(['p_code' =>  $promocode])->one();
-                if(!$query) {
-                    $query = 'Empty';
-                }
-                else {
-                    $discount = $query['p_discount'];
-                    if($query['p_type'] == 'F') {
-                        if($count > $query['p_quantity'] || $total > $query['p_total']) {
-                            $query = 'Success '.$query['p_code'];
-                            self::$cart_total['discount'] = $discount;
-                            self::$cart_total['total_b'] = $total - $discount;
-                        }
-                    }
-                    else if($query['p_type'] == 'P') {
-                        if($count > $query['p_quantity'] || $total > $query['p_total']) {
-                            $query = 'Success '.$query['p_code'];
-                            self::$cart_total['discount'] = $discount;
-                            self::$cart_total['total_b'] = ($total * ( 100 - $discount)) / 100;
-                        }
-                    }
-                }
-                if($post['country'] == 1) {
-                    if($count < 2 || $total < 150) {
-                        self::$cart_total['shipping'] = self::$MALAYSIA;
-                    }
-                }
-                else if($post['country'] == 2) {
-                    if($total < 300) {
-                        self::$cart_total['shipping'] = self::$SINGAPORE;
-                    }
-                } 
-                else if($post['country'] == 3) {
-                    if($total < 300) {
-                        self::$cart_total['shipping'] = self::$BRUNEI;
-                    }
-                }
-            }
-        }
-        self::$cart_total['total'] = self::$cart_total['total_b'] + self::$cart_total['shipping'];
         /*if ($model) {
             Yii::$app->cart->put($model, 1);
             return $this->render('cart',['carts' => $cart]);
         }
         throw new NotFoundHttpException();*/
 
-        return $this->render('cart',['carts' => $model, 'post' => $post, 'query' => $query, 'cart_total' => self::$cart_total]);
+        return $this->render('cart',['carts' => $model, 'post' => $post, 'query' => $query, 'cart_total' => Product::getCartTotal()]);
     }
 
     public function actionSummary()
--- models/Product.php
+++ models/Product.php
@@ -2,12 +2,23 @@
 
 namespace app\models;
 
+use Yii;
 use yii\db\ActiveRecord;
 use yz\shoppingcart\CartPositionInterface;
 use yz\shoppingcart\CartPositionTrait;
 
 class Product extends ActiveRecord implements CartPositionInterface
 {
+    protected static $MALAYSIA = 10;
+    protected static $SINGAPORE = 20;
+    protected static $BRUNEI = 25;
+
+    protected static $cart_total = array (
+        'total' => 0,
+        'total_b' => 0,
+        'shipping' => 0,
+        'discount' => 0
+    );
 
     use CartPositionTrait;
 
@@ -25,4 +36,59 @@ class Product extends ActiveRecord implements CartPositionInterface
     {
         return $this->p_price;
     }
+
+    public function calculateCart($post) {
+        
+        $count = Yii::$app->cart->getCount();
+        $total = Yii::$app->cart->getCost();
+        self::$cart_total['total_b'] = $total;
+        if($post) {
+            if($post['validate']) {
+                $promocode = strtoupper($post['promocode']);
+                $country = $post['country'];
+                $query = Promocode::find()->where(['p_code' =>  $promocode])->one();
+                if(!$query) {
+                    $query = 'Empty';
+                }
+                else {
+                    $discount = $query['p_discount'];
+                    if($query['p_type'] == 'F') {
+                        if($count > $query['p_quantity'] || $total > $query['p_total']) {
+                            $query = 'Success '.$query['p_code'];
+                            self::$cart_total['discount'] = $discount;
+                            self::$cart_total['total_b'] = $total - $discount;
+                        }
+                    }
+                    else if($query['p_type'] == 'P') {
+                        if($count > $query['p_quantity'] || $total > $query['p_total']) {
+                            $query = 'Success '.$query['p_code'];
+                            self::$cart_total['discount'] = $discount;
+                            self::$cart_total['total_b'] = ($total * ( 100 - $discount)) / 100;
+                        }
+                    }
+                }
+                if($post['country'] == 1) {
+                    if($count < 2 || $total < 150) {
+                        self::$cart_total['shipping'] = self::$MALAYSIA;
+                    }
+                }
+                else if($post['country'] == 2) {
+                    if($total < 300) {
+                        self::$cart_total['shipping'] = self::$SINGAPORE;
+                    }
+                } 
+                else if($post['country'] == 3) {
+                    if($total < 300) {
+                        self::$cart_total['shipping'] = self::$BRUNEI;
+                    }
+                }
+            }
+        }
+        self::$cart_total['total'] = self::$cart_total['total_b'] + self::$cart_total['shipping'];
+    }
+
+    public function getCartTotal() {
+        return self::$cart_total;
+    }
+
 }
